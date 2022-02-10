961340dc76d4fbb4724414861b243a93ec1779a3
Elton Wang <elton.wang@wpgholdings.com>
2022-02-10 10:16:15 +0800

    translate to chinese

--- index.js
+++ index.js
@@ -188,17 +188,17 @@ function convertMarkdown(str) {
 
 function getFloatingNavItems(resume) {
     const floating_nav_items = [
-        {label: 'About', target: 'about', icon: 'board', requires: 'basics.summary'},
-        {label: 'Work Experience', target: 'work-experience', icon: 'office', requires: 'work'},
-        {label: 'Projects Experience', target: 'projects-experience', icon: 'code', requires: 'projects'},
-        {label: 'Skills', target: 'skills', icon: 'tools', requires: 'skills'},
-        {label: 'Education', target: 'education', icon: 'graduation-cap', requires: 'education'},
-        {label: 'Certificates', target: 'certificates', icon: 'profile', requires: 'certificates'},
-        {label: 'Awards', target: 'awards', icon: 'trophy', requires: 'awards'},
-        {label: 'Volunteer Work', target: 'volunteer-work', icon: 'child', requires: 'volunteer'},
-        {label: 'Publications', target: 'publications', icon: 'newspaper', requires: 'publications'},
-        {label: 'Interests', target: 'interests', icon: 'heart', requires: 'interests'},
-        {label: 'References', target: 'references', icon: 'thumbs-up', requires: 'references'}
+        {label: '自我介紹', target: 'about', icon: 'board', requires: 'basics.summary'},
+        {label: '工作經驗', target: 'work-experience', icon: 'office', requires: 'work'},
+        {label: '專案經驗', target: 'projects-experience', icon: 'code', requires: 'projects'},
+        {label: '技能專長', target: 'skills', icon: 'tools', requires: 'skills'},
+        {label: '教育背景', target: 'education', icon: 'graduation-cap', requires: 'education'},
+        {label: '專業認證', target: 'certificates', icon: 'profile', requires: 'certificates'},
+        {label: '得獎紀錄', target: 'awards', icon: 'trophy', requires: 'awards'},
+        {label: '志工服務', target: 'volunteer-work', icon: 'child', requires: 'volunteer'},
+        {label: '出版刊物', target: 'publications', icon: 'newspaper', requires: 'publications'},
+        {label: '興趣喜好', target: 'interests', icon: 'heart', requires: 'interests'},
+        {label: '自由推薦', target: 'references', icon: 'thumbs-up', requires: 'references'}
     ];
 
     return _(floating_nav_items).filter(item => {
